56b0661e27d42a0e042f6158bb61ec3909cff146
Wookiee- <taggart_james@hotmail.com>
2020-06-24 09:21:25 -0400

    Minor

--- codemp/game/g_local.h
+++ codemp/game/g_local.h
@@ -44,7 +44,7 @@ extern vec3_t gPainPoint;
 //==================================================================
 
 // the "gameversion" client command will print this plus compile date
-#define	GAMEVERSION	"japro"
+#define	GAMEVERSION	"OpenJK"
 
 #define SECURITY_LOG "security.log"
 
